1a03870f864300c7305a5f05ac744448b559cbd2
dok53 <derekokeeffe85@yahoo.ie>
2015-12-02 14:40:43 +0000

    Moving to sign up

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,17 +39,25 @@ class UserTest < ActiveSupport::TestCase
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
+  end
     
     test "email validation should reject invalid addresses" do
-      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses = %w[user@example,com user_at_foo.org foo@bar..com user.name@example.foo@bar_baz.com foo@bar+baz.com]
       invalid_addresses.each do |invalid_address|
         @user.email = invalid_address
         assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
       end
+    end
+    
+    test "email addresses should be saved as lower-case" do
+      mixed_case_email = "Foo@ExAMPle.CoM"
+      @user.email = mixed_case_email
+      @user.save
+      assert_equal mixed_case_email.downcase, @user.reload.email
+    end
       
       test "email address should be unique" do
-        duplicate_user = @user.duplicate_user
-        duplicate_user.email = @user.email.upcase
+        duplicate_user = @user.dup
         @user.save
         assert_not duplicate_user.valid?
       end 
@@ -63,7 +71,4 @@ class UserTest < ActiveSupport::TestCase
         @user.password = @user.password_confirmation = "a" * 5
         assert_not @user.valid?
       end
-        
-    end
-  end
 end
